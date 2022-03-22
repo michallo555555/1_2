b6d05d3ada3753ddf7f0496c014bab1839f1df03
michallo555555 <101186827+michallo555555@users.noreply.github.com>
2022-03-22 11:43:04 +0000

    final

--- Main.java
+++ Main.java
@@ -1,5 +1,14 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    int i=1;
+    char s=2;
+    double c=3;
+    String str = "Java";    
+    
+    
+    System.out.println(i);
+      System.out.println(s);
+      System.out.println(c);
+          System.out.println(str);
   }
 }
